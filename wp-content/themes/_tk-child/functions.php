062b4712500499273b0ddb82ce0b44f9e46addaf
idanis gallimore <miamigirl2017@outlook.com>
2017-08-04 00:24:23 +0000

    Finally done, at least it meets the requirements ;)

--- wp-content/themes/_tk-child/functions.php
+++ wp-content/themes/_tk-child/functions.php
@@ -14,6 +14,8 @@
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     
     }
+    
+    require('functions2.php');
 
     /**
      * This is an example of usage for the Eloquent ORM
@@ -30,5 +32,158 @@
      */
      
      
+     /** 
+                *********************************************
+                *                                           *
+                *                                           *
+                *   HERE THE DISASTER BEGINS                *
+                *                                           *
+                *                                           *
+                *********************************************
+     */
      
-?>
+ 
+    /*  Creating the Post Type LESSON */
+    add_action( 'init', 'my_custom_post_lesson' );
+    
+    function my_custom_post_lesson() {
+        
+        $labels = array(
+            'name'               => _x( 'Lessons', 'post type general name' ),
+            'singular_name'      => _x( 'Lesson', 'post type singular name' ),
+            'add_new'            => _x( 'Add New', 'Lesson' ),
+            'add_new_item'       => __( 'Add New Lesson' ),
+            'edit_item'          => __( 'Edit Lesson' ),
+            'new_item'           => __( 'New Lesson' ),
+            'all_items'          => __( 'All Lessons' ),
+            'view_item'          => __( 'View Lesson' ),
+            'search_items'       => __( 'Search Lessons' ),
+            'not_found'          => __( 'No Lessons found' ),
+            'not_found_in_trash' => __( 'No Lessons found in the Trash' ),
+            'parent_item_colon'  => 'Course: ',
+            'menu_name'          => 'Lessons'
+          );
+        
+        $args = array(
+            'labels'        => $labels,
+            'description'   => 'Displays lessons and their ratings',
+            'public'        => true,
+            'menu_position' => 1,
+            // 'hierarchical' => true,
+            'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+            'has_archive'   => true,
+            'menu_icon'   => 'dashicons-media-document',
+            'taxonomies' => array()
+            
+          );
+  
+        register_post_type( 'lesson', $args );
+    }
+     
+     
+    // creating the extended fields so Admin can enroll students in Courses (taxonomy Course)
+    add_action( 'show_user_profile', 'enrolled_field' );
+    add_action( 'edit_user_profile', 'enrolled_field' );
+    add_action( 'profile_update', 'save_enrolled_field' );
+    
+    function enrolled_field() {
+        // If is current user's profile (profile.php)
+        if ( defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE ) {
+            $user_id = get_current_user_id();
+        // If is another user's profile page
+        } elseif (! empty($_GET['user_id']) && is_numeric($_GET['user_id']) ) {
+            $user_id = $_GET['user_id'];
+        // Otherwise something is wrong.
+        } else {
+            die( 'No user id defined.' );
+        }
+
+        $selected_courses = get_user_meta( $user_id, '_enrolled', true );
+        $all_courses = get_terms( array(
+            'taxonomy' => 'course',
+            'order' => 'DESC',
+            'hide_empty' => false
+        ) );
+        
+        ?>
+        
+        <!--View elements of the additional fields-->
+        <h3>Extra profile information</h3>
+
+    	<table class="form-table courses-table">
+    		<tr>
+    			<th><label>Enrolled in Courses <br/>User ID: <?php echo $user_id ?></label></th>
+    			<td>
+    			    <button type="button" id="clear_all_courses" class="button button-primary" onclick="
+    			    <?php echo "document.querySelectorAll('table.courses-table .checkbox input').forEach(function(item){item.checked = false});" ?>">
+    			        Remove from all Courses
+    			    </button> 
+    			    
+    				<ul>
+                        <?php foreach ( $all_courses as $course ) : ?>
+                            <li><div class="checkbox"><label><input type="checkbox" name="enrolled[]" value="<?php echo $course->term_id ?>" <?php echo in_array($course->term_id , $selected_courses) ? ' checked' : ''; ?> ><?php echo $course->term_id . ' - ' .  ucwords($course->name) ; ?></label></div></li>
+                        <?php endforeach; ?>
+                    </ul>
+    			</td>
+    		</tr>
+    	</table>
+        
+     <?php   
+    }
+    
+    function save_enrolled_field( $user_id ) {
+    
+        if (!is_null($_POST['enrolled'])){
+            update_user_meta( $user_id, '_enrolled', $_POST['enrolled'] );
+        }
+        else{
+            update_user_meta( $user_id, '_enrolled', array() );
+        }
+    
+    }
+
+
+
+    // Creating COURSE taxonomy
+    add_action( 'init', 'create_course_taxonomies', 0 );
+    
+    function create_course_taxonomies() {
+    	// Add new taxonomy, make it hierarchical (like categories)
+    	$labels = array(
+    		'name'              => 'Courses',
+    		'singular_name'     => 'Course',
+    		'search_items'      => 'Search Courses',
+    		'all_items'         => 'All Courses',
+    		'parent_item'       => 'Parent Course',
+    		'parent_item_colon' => 'Parent Course:',
+    		'edit_item'         => 'Edit Course',
+    		'update_item'       => 'Update Course',
+    		'add_new_item'      => 'Add New Course',
+    		'new_item_name'     => 'New Course Name',
+    		'menu_name'         => 'Courses',
+    	);
+    
+    	$args = array(
+    		'hierarchical'      => true,
+    		'labels'            => $labels,
+    		'show_ui'           => true,
+    		'show_admin_column' => true,
+    		'query_var'         => true,
+    		'rewrite'           => array( 'slug' => 'course' ),
+    	);
+    
+    	register_taxonomy( 'course', array( 'lesson' ), $args );
+    }
+
+
+    
+    // redirect user to homepage after logging out
+    add_action('wp_logout','auto_redirect_after_logout');
+    function auto_redirect_after_logout(){
+      wp_redirect( home_url() );
+      exit();
+    }
+    
+    
+    //hide the admin bar in the front end
+    add_filter('show_admin_bar', '__return_false');
--- wp-content/themes/_tk-child/home.php
+++ wp-content/themes/_tk-child/home.php
@@ -1,23 +1,84 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Team Trump!</title>
-        
-        <?php wp_head(); ?>
-    </head>
-    <body>
-        
-<?php    
-    wp_nav_menu( array(
-        'menu' => 'Default',
-        'depth' => 2,
-        'container' => false,
-        'menu_class' => 'nav',
-//Process nav menu using our custom nav walker
-    'walker' => new wp_bootstrap_navwalker())
-    );
- ?>
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _tk
+ */
 
-        <?php wp_footer(); ?>
-    </body>
-</html>
+get_header(); ?>
+
+
+	<div class="jumbotron text-center">
+          
+          <?php 
+          
+            if ( is_user_logged_in() ) {
+               
+                $current_user = wp_get_current_user();
+                echo "<h1>Howdy <strong>" . esc_html( $current_user->user_nicename ) . '</strong></h1><hr/><p><a class="btn btn-success btn-lg" href="'  . get_page_link(26) . '" role="button">MY COURSES</a></p>';
+                echo "<h5>" . get_search_form() . "</h5>";
+            } else {
+                ?>
+                
+                <h1 style="padding: 50px 0;">Welcome to Open Academy!</h1>
+                
+                <?php
+            }
+          
+          ?>
+    </div>
+
+
+
+	<section class="container">
+            
+            
+            <div class="row">
+                
+                <?php $args = array(
+                	'sort_order' => 'asc',
+                	'sort_column' => 'post_title',
+                	'hierarchical' => 1,
+                	'exclude' => '',
+                	'include' => '',
+                	'meta_key' => '',
+                	'meta_value' => '',
+                	'authors' => '',
+                	'child_of' => 0,
+                	'parent' => -1,
+                	'exclude_tree' => '',
+                	'number' => '3',
+                	'offset' => 0,
+                	'post_type' => 'page',
+                	'post_status' => 'publish'
+                ); 
+                
+                $pages = get_pages($args); 
+                
+                // print_r($pages);
+                
+                foreach ($pages as $page) {
+                    echo "<div class='col-sm-4 col-xs-12 text-center'>";
+                    
+                    echo "<h3><a href='" . $page->guid . "'>" . $page->post_title . "</a></h3>";
+                    
+                    echo "</div>";
+                    
+                }
+                ?>
+            
+               </div>
+                
+            </div>
+            
+        </section>
+        
+        
+
+<?php get_footer(); ?>
